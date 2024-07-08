29144b22bc71bc8a889623eca6dc742a34b6b959
Aditi Sagar <hireaditisagar@gmail.com>
2024-07-07 23:37:22 -0700

    Create worker_pool.go

---
+++ concurrency/worker_pool.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+func worker(id int, jobs <-chan int, wg *sync.WaitGroup) {
+    defer wg.Done()
+    for job := range jobs {
+        fmt.Printf("Worker %d is processing job %d\n", id, job)
+        // Perform your work here.
+    }
+}
+
+func main() {
+    numWorkers := 3
+    numJobs := 10
+    jobs := make(chan int, numJobs)
+    var wg sync.WaitGroup
+
+    for i := 1; i <= numWorkers; i++ {
+        wg.Add(1)
+        go worker(i, jobs, &wg)
+    }
+
+    for i := 1; i <= numJobs; i++ {
+        jobs <- i
+    }
+    close(jobs)
+
+    wg.Wait()
+}
